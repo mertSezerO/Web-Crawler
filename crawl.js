3e6b47e581223426533166b7fe366467f03514a5
mertSezerO <117273151+mertSezerO@users.noreply.github.com>
2023-02-19 19:15:16 +0300

    normalizeURLfromHTML function and its tests are implemented.

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const {JSDOM} = require('jsdom')
+
 function normalizeURL(inputURL){
     const url = new URL(inputURL)
     const hostPath = '' +url.hostname + url.pathname
@@ -7,6 +9,29 @@ function normalizeURL(inputURL){
    return hostPath
 }
 
+function normalizeURLfromHTML(html, baseURL){
+    const urls = []
+    const dom = new JSDOM(html)
+    const links = dom.window.document.querySelectorAll('a')
+    for (const link of links){
+        try{
+            if(link.href.slice(0,1) === '/'){
+                const url = new URL(baseURL + link.href)
+                urls.push(url.href)
+            }
+            else{
+                const url = new URL(link.href)
+                urls.push(url.href)
+            } 
+        }
+        catch(err){
+            console.log("Error with url: " + err.message)
+        }
+    }
+    return urls
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    normalizeURLfromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL,normalizeURLfromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL strip', () => {
@@ -26,5 +26,58 @@ test('normalizeURL strip for /', () => {
     const input = 'https://blog.boot.dev/path/'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected) 
+})
+
+test('normalizeURLfromHTML absolute url', () => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/">Boot.dev Blog</a>
+        </body>
+    </html>`
+    const inputURL = "https://blog.boot.dev"
+    const actual = normalizeURLfromHTML(inputHTML,inputURL)
+    const expected = ["https://blog.boot.dev/"]
+    expect(actual).toEqual(expected) 
+})
+
+test('normalizeURLfromHTML relative url', () => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="/path/">Boot.dev Blog</a>
+        </body>
+    </html>`
+    const inputURL = "https://blog.boot.dev"
+    const actual = normalizeURLfromHTML(inputHTML,inputURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected) 
+})
+
+test('normalizeURLfromHTML multiple links', () => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="/path/">Boot.dev Blog</a>
+            <a href="/home/">Home</a>
+        </body>
+    </html>`
+    const inputURL = "https://blog.boot.dev"
+    const actual = normalizeURLfromHTML(inputHTML,inputURL)
+    const expected = ["https://blog.boot.dev/path/","https://blog.boot.dev/home/"]
+    expect(actual).toEqual(expected) 
+})
+
+test('normalizeURLfromHTML invalid link', () => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="invalid">Invalid URL</a>
+        </body>
+    </html>`
+    const inputURL = "https://blog.boot.dev"
+    const actual = normalizeURLfromHTML(inputHTML,inputURL)
+    const expected = []
     expect(actual).toEqual(expected) 
 })
